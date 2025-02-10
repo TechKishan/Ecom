d27ffbdeff1460637119afc29dc091773a7bd95b
Kishan <kishanarustu@gmail.com>
2025-02-10 16:43:34 +0530

    home page

---
+++ Ecom/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using static Ecom.Common;
+
+namespace Ecom.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        [HttpPost]
+        [Route("api/Orders/Insert")]
+        public MessageFor Insert(Order data)
+        {
+            return Models.Orders.OrderInsert(data);
+        }
+    }
+}
---
+++ Ecom/Models/Orders.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using static Ecom.Common;
+
+namespace Ecom.Models
+{
+    public class Orders
+    {
+        public static MessageFor OrderInsert(Order data)
+        {
+            try
+            {
+                SqlConnection sql = new SqlConnection(DBConnection.cs);
+                SqlCommand cmd = new SqlCommand("Sp_Orders", sql);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@Action", "INSERT"));
+                cmd.Parameters.Add(new SqlParameter("@UserId", data.UserId));
+                cmd.Parameters.Add(new SqlParameter("@ShippingAddress", data.ShippingAddress));
+                cmd.Parameters.Add(new SqlParameter("@TotalAmount", data.TotalAmount));
+                sql.Open();
+                cmd.ExecuteNonQuery();
+                sql.Close();
+                return new MessageFor
+                {
+                    Status = 1,
+                    Message = "Order created Successfully."
+                };
+            }
+            catch (Exception ex)
+            {
+                return new MessageFor
+                {
+                    Status = 0,
+                    Message = "Something went wrong.",
+                };
+            }
+        }
+    }
+}
+public class Order
+{
+    public int OrderId { get; set; }
+    public int UserId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string ShippingAddress { get; set; }
+    public string TotalAmount { get; set; }
+    public string OrderStatus { get; set; }
+    public string PaymentStatus { get; set; }
+}
