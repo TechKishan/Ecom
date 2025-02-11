38c6b5578ddb1aa8873d47cb28b605f4c4b605f1
Kishan <kishanarustu@gmail.com>
2025-02-11 16:43:59 +0530

    Order changes in details section

--- Ecom/Controllers/OrderDetailsController.cs
+++ Ecom/Controllers/OrderDetailsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -15,6 +16,27 @@ namespace Ecom.Controllers
         public MessageFor AddOrder(OrderDetail data)
         {
             return Models.OrderDetails.AddOrder(data);
+        }
+
+        [HttpPost]
+        [Route("api/OrderDetails/UpdateOrder")]
+        public MessageFor UpdateOrder(OrderDetail data)
+        {
+            return Models.OrderDetails.UpdateOrder(data);
+        }
+
+        [HttpPost]
+        [Route("api/OrderDetails/DeleteOrder")]
+        public MessageFor DeleteOrder(OrderDetail data)
+        {
+            return Models.OrderDetails.DeleteOrder(data);
+        }
+
+        [HttpPost]
+        [Route("api/OrderDetails/GetOrderDetails")]
+        public DataTable GetOrderDetails(OrderDetail data)
+        {
+            return Models.OrderDetails.GetOrderDetails(data);
         }
     }
 }
--- Ecom/Models/OrderDetails.cs
+++ Ecom/Models/OrderDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -36,6 +37,75 @@ namespace Ecom.Models
                     Message = "error."
                 };
             }
+        }
+
+        public static MessageFor UpdateOrder(OrderDetail data)
+        {
+            try
+            {
+                SqlParameter[] para = new SqlParameter[] {
+                new SqlParameter("@Action","UPDATE"),
+                new SqlParameter("@OrderDetailId",data.OrderDetailId),
+                new SqlParameter("@Quantity",data.Quantity),
+                new SqlParameter("@Price",data.Price)
+            };
+                DBConnection dB = new DBConnection();
+                dB.ExecuteNonQuery("Sp_OrderDetails", para);
+                return new MessageFor
+                {
+                    Status = 1,
+                    Message = "Order Updated."
+                };
+            }
+            catch (Exception ex)
+            {
+                return new MessageFor
+                {
+                    Status = 1,
+                    Message = "error."
+                };
+            }
+        }
+
+        public static MessageFor DeleteOrder(OrderDetail data)
+        {
+            try
+            {
+                SqlParameter[] para = new SqlParameter[] {
+                new SqlParameter("@Action","DELETE"),
+                new SqlParameter("@OrderDetailId",data.OrderDetailId),
+            };
+                DBConnection dB = new DBConnection();
+                dB.ExecuteNonQuery("Sp_OrderDetails", para);
+                return new MessageFor
+                {
+                    Status = 1,
+                    Message = "Order Deleted."
+                };
+            }
+            catch (Exception ex)
+            {
+                return new MessageFor
+                {
+                    Status = 1,
+                    Message = "error."
+                };
+            }
+        }
+
+        public static DataTable GetOrderDetails(OrderDetail data)
+        {
+            SqlConnection sql = new SqlConnection(DBConnection.cs);
+            SqlCommand cmd = new SqlCommand("Sp_OrderDetails", sql);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@Action", "GET"));
+            cmd.Parameters.Add(new SqlParameter("@OrderId",data.OrderId));
+            DataTable dt=new DataTable();
+            SqlDataAdapter adt = new SqlDataAdapter(cmd);
+            sql.Open(); 
+            adt.Fill(dt);
+            sql.Close();
+            return dt;
         }
     }
 }
